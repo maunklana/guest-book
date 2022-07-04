91c599957552f9048d6d7cdc8ce4129605fc5eef
chickenin <maunklana@users.noreply.github.com>
2022-07-04 23:52:14 +0700

    change files order

--- functions.js
+++ functions.js
@@ -7,3 +7,90 @@ function parseJwt (token) {
 
     return JSON.parse(jsonPayload);
 };
+
+const swallAskLogin = function(functiontoCall){
+	Swal.fire({
+		html: '<div class="text-light pb-2"><h1><i class="bi bi-person-bounding-box"></i></h1>Kamu harus login untuk mengakses halaman ini!</div><div id="googleLoginButton"></div>',
+		color: "white",
+		showCancelButton: false,
+		confirmButtonText: 'Lanjut',
+		confirmButtonColor: '#991188', //Warna kesukaan Nabila
+		background: 'transparent',
+		backdrop: `
+		linear-gradient(rgba(255, 255, 255, 0.5), rgba(100, 100, 100, 1))
+		`,
+		allowOutsideClick: false,
+		allowEscapeKey: false,
+		showConfirmButton: false,
+		didOpen: () => {
+			google.accounts.id.initialize({
+				client_id: "12760851327-a1i1d9s9gjg3d8fnvj0t2hken7gpter1.apps.googleusercontent.com",
+				callback: handleCredentialResponse
+			});
+			google.accounts.id.renderButton(
+				document.getElementById("googleLoginButton"),
+				{ theme: "outline", size: "large" }  // customization attributes
+			);
+			google.accounts.id.prompt(); // also display the One Tap dialog
+		}
+	}).then((result) => {
+		showGuestBooks();
+	});
+}
+
+function handleCredentialResponse(response) {
+	console.log("Encoded JWT ID token: " + response.credential);
+	
+	const responsePayload = parseJwt(response.credential);
+	console.log("ID: " + responsePayload.sub);
+	console.log('Full Name: ' + responsePayload.name);
+	console.log('Given Name: ' + responsePayload.given_name);
+	console.log('Family Name: ' + responsePayload.family_name);
+	console.log("Image URL: " + responsePayload.picture);
+	console.log("Email: " + responsePayload.email);
+	
+	localStorage.googleCredentials = response.credential;
+	showGuestBooks();
+}
+
+const showGuestBooks = function(){
+	googleCredentials = localStorage.googleCredentials;
+	
+	$('.xhidden').each(function() {
+		$(this).addClass('animate__animated animate__slideInUp');
+		$(this).css('visibility', 'visible');
+	});
+	
+	loadGuestBooks();
+}
+
+const loadGuestBooks = function(){
+	$.getJSON( 'https://script.google.com/macros/s/AKfycbyFeS9ghi4Cj44eguhffRmT1bqHrI94mYLA3pS6fjXpW5YokJq7GIAojYCp-VIaBKic/exec?action=getGuestBook&vc1ycvwbf6zuqyn1cf=true' ).done((response) => {
+		$("#table-loader").addClass("d-none");
+		if(response.statusCode == 1){
+			$("#guestbooks-datalist").html("");
+			// split into lines
+			let rows = response.data.split("\n");
+			// parse lines
+			rownum = 0;
+			rows.forEach( function getvalues(ourrow) {
+				//Skip header
+				if(rownum>0){
+					guestbookrows = $("<tr>");
+
+					// split line into columns
+					let columns = ourrow.split(",");
+					
+					$.each(columns, function( index, value ) {
+						guestbookrows.append(`<td>${value}</td>'`);
+					});
+					
+					$("#guestbooks-datalist").append(guestbookrows);
+				}
+				rownum++;
+			});
+		}else{
+			$("#guestbooks-datalist").append(response.statusText);
+		}
+	});
+}
