5e9a085eced3fca11e40842f9f5baa6ab9e6a4e8
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-05 18:18:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(window).on("load", function() {
 	google.accounts.id.initialize({
 		client_id: "12760851327-a1i1d9s9gjg3d8fnvj0t2hken7gpter1.apps.googleusercontent.com",
 		callback: handleGoogleCredentialResponse
