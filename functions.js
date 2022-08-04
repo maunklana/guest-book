1539fb0dbc5a00d5a18a9ae6c2d1c8363b612a7a
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-08-04 17:22:09 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -104,8 +104,7 @@ const showGuestBooks = function(){
 			$("#addguestbookmanual").submit(function(e){
 				e.preventDefault();
 				$("#addguestbookmanual :input").prop("disabled", true); 
-				
-				$.ajax({
+				$requestSettings = {
 					"url": "https://script.google.com/macros/s/AKfycbyFeS9ghi4Cj44eguhffRmT1bqHrI94mYLA3pS6fjXpW5YokJq7GIAojYCp-VIaBKic/exec?action=insertGuestToBook",
 					"method": "POST",
 					"timeout": 0,
@@ -114,7 +113,8 @@ const showGuestBooks = function(){
 					},
 					"redirect": "follow",
 					"data": $("#addguestbookmanual").serialize()
-				}).done(function (response) {
+				};
+				$.ajax($requestSettings).done(function (response) {
 					$("#addguestbookmanual :input").prop("disabled", false);
 					
 					infotext = 'Data gagal disimpan, silahkan coba kembali.';
