eee78fb46e26b9d98574e47247774e6d70bc08fc
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-08-04 15:01:23 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -102,6 +102,7 @@ const showGuestBooks = function(){
 	}
 }
 
+let currentGuestNum = 0;
 const loadGuestBooks = function(){
 	$.getJSON( 'https://script.google.com/macros/s/AKfycbyFeS9ghi4Cj44eguhffRmT1bqHrI94mYLA3pS6fjXpW5YokJq7GIAojYCp-VIaBKic/exec?action=getGuestBook&vc1ycvwbf6zuqyn1cf=true' ).done((response) => {
 		$("#table-loader").addClass("d-none");
@@ -109,24 +110,28 @@ const loadGuestBooks = function(){
 			$("#guestbooks-datalist").html("");
 			// split into lines
 			let rows = response.data.split("\n");
-			// parse lines
-			rownum = 0;
-			rows.forEach( function getvalues(ourrow) {
-				//Skip header
-				if(rownum>0){
-					guestbookrows = $("<tr>");
+			if(currentGuestNum<rows.length){
+				// parse lines
+				rownum = 0;
+				rows.forEach( function getvalues(ourrow) {
+					//Skip header
+					if(rownum>0){
+						guestbookrows = $("<tr>");
 
-					// split line into columns
-					let columns = ourrow.split(",");
-					
-					$.each(columns, function( index, value ) {
-						guestbookrows.append(`<td>${value}</td>'`);
-					});
-					
-					$("#guestbooks-datalist").append(guestbookrows);
-				}
-				rownum++;
-			});
+						// split line into columns
+						let columns = ourrow.split(",");
+						
+						$.each(columns, function( index, value ) {
+							guestbookrows.append(`<td>${value}</td>'`);
+						});
+						
+						$("#guestbooks-datalist").append(guestbookrows);
+					}
+					rownum++;
+				});
+				
+				currentGuestNum = rows.length;
+			}
 			
 			setTimeout(loadGuestBooks, 1000);
 		}else{
